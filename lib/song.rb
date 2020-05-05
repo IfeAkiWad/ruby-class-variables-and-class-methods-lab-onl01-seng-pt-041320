0708023a9bfbeab401fafcb91f599611d457e38d
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-05-05 00:15:23 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+require 'Pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+
+  def initialize
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres = []
+
+  end
+
+
+end
